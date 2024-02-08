541690af1dfd728eff0972aac8ed823048f15bea
idky137 <idky137@works.local>
2024-02-08 10:39:57 +0000

    added main,

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use http::Uri;
 use proxy::{spawn_server, ProxyServer};
-
+use std::thread;
+use std::time::Duration;
 mod proxy;
 
 #[tokio::main]
 async fn main() {
-    // spawn_server().await.unwrap().unwrap()
+    let server_port = 8080;
+    let server_handle = spawn_server(server_port, 9067, 18232).await;
+    loop {
+        thread::sleep(Duration::from_secs(10));
+    }
 }
