5193209292cb0bbe74cd94fc3adf4f9f6302234b
idky137 <idky137@gmail.com>
2024-05-13 17:46:48 +0100

    removed e2e.rs

--- integration-tests/tests/e2e.rs
+++
@@ -1,7 +0,0 @@
-//! End to end tests for zingo-Proxy.
-//!
-//! Uses ZingoLib as its test wallet. Currently uses ZCashD as ZebraD has not yet implemented Regtest Mode.
-
-#![forbid(unsafe_code)]
-
-// TODO: Update Zingolib scenario builder for consumption here.
