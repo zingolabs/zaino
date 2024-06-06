af5cc8a6bb8c2bd8099f0e839918f77751e28d53
idky137 <idky137@gmail.com>
2024-06-06 16:49:48 +0100

    added block and transaction structs

--- zingo-rpc/src/blockcache/block.rs
+++ zingo-rpc/src/blockcache/block.rs
@@ -1,30 +1,107 @@
 //! Block fetching and deserialization functionality.
 
-/// Block header data.
+/// A block header, containing metadata about a block.
+///
+/// How are blocks chained together? They are chained together via the
+/// backwards reference (previous header hash) present in the block
+/// header. Each block points backwards to its parent, all the way
+/// back to the genesis block (the first block in the blockchain).
 #[derive(Debug)]
 pub struct BlockHeaderData {
+    /// The block's version field. This is supposed to be `4`:
+    ///
+    /// > The current and only defined block version number for Zcash is 4.
+    ///
+    /// but this was not enforced by the consensus rules, and defective mining
+    /// software created blocks with other versions, so instead it's effectively
+    /// a free field. The only constraint is that it must be at least `4` when
+    /// interpreted as an `i32`.
+    ///
+    /// Size [bytes]: 4
     pub version: i32,
+
+    /// The hash of the previous block, used to create a chain of blocks back to
+    /// the genesis block.
+    ///
+    /// This ensures no previous block can be changed without also changing this
+    /// block's header.
+    ///
+    /// Size [bytes]: 32
     pub hash_prev_block: Vec<u8>,
+
+    /// The root of the Bitcoin-inherited transaction Merkle tree, binding the
+    /// block header to the transactions in the block.
+    ///
+    /// Note that because of a flaw in Bitcoin's design, the `merkle_root` does
+    /// not always precisely bind the contents of the block (CVE-2012-2459). It
+    /// is sometimes possible for an attacker to create multiple distinct sets of
+    /// transactions with the same Merkle root, although only one set will be
+    /// valid.
+    ///
+    /// Size [bytes]: 32
     pub hash_merkle_root: Vec<u8>,
+
+    /// [Pre-Sapling] A reserved field which should be ignored.
+    /// [Sapling onward] The root LEBS2OSP_256(rt) of the Sapling note
+    /// commitment tree corresponding to the final Sapling treestate of this
+    /// block.
+    ///
+    /// Size [bytes]: 32
     pub hash_final_sapling_root: Vec<u8>,
+
+    /// The block timestamp is a Unix epoch time (UTC) when the miner
+    /// started hashing the header (according to the miner).
+    ///
+    /// Size [bytes]: 4
     pub time: u32,
+
+    /// An encoded version of the target threshold this block's header
+    /// hash must be less than or equal to, in the same nBits format
+    /// used by Bitcoin.
+    ///
+    /// For a block at block height `height`, bits MUST be equal to
+    /// `ThresholdBits(height)`.
+    ///
+    /// [Bitcoin-nBits](https://bitcoin.org/en/developer-reference#target-nbits)
+    ///
+    /// Size [bytes]: 4
     pub n_bits_bytes: Vec<u8>,
+
+    /// An arbitrary field that miners can change to modify the header
+    /// hash in order to produce a hash less than or equal to the
+    /// target threshold.
+    ///
+    /// Size [bytes]: 32
     pub nonce: Vec<u8>,
+
+    /// The Equihash solution.
+    ///
+    /// Size [bytes]: CompactLength
     pub solution: Vec<u8>,
 }
 
 /// Complete block header.
 #[derive(Debug)]
 pub struct FullBlockHeader {
+    /// Block header data.
     pub raw_block_header: BlockHeaderData,
+
+    /// Hash of the current block.
     pub cached_hash: Vec<u8>,
 }
 
 /// Zingo-Proxy Block.
 #[derive(Debug)]
 pub struct FullBlock {
+    /// The block header, containing block metadata.
+    ///
+    /// Size [bytes]: 140+CompactLength
     pub hdr: FullBlockHeader,
+
+    /// The block transactions.
     pub vtx: Vec<super::transaction::FullTransaction>,
+
+    /// Block height.
     pub height: i32,
 }
 
--- zingo-rpc/src/blockcache/transaction.rs
+++ zingo-rpc/src/blockcache/transaction.rs
@@ -1,65 +1,168 @@
 //! Transaction fetching and deserialization functionality.
 
-/// Transparent input.
+/// Txin format as described in https://en.bitcoin.it/wiki/Transaction
 #[derive(Debug)]
 pub struct TxIn {
+    // PrevTxHash [IGNORED] - Size[bytes]: 32
+    // PrevTxOutIndex [IGNORED] - Size[bytes]: 4
+    /// CompactSize-prefixed, could be a pubkey or a script
+    ///
+    /// Size[bytes]: CompactSize
     pub script_sig: Vec<u8>,
+    // SequenceNumber [IGNORED] - Size[bytes]: 4
 }
 
-/// Transparent output.
+/// Txout format as described in https://en.bitcoin.it/wiki/Transaction
 #[derive(Debug)]
 pub struct TxOut {
+    /// Non-negative int giving the number of zatoshis to be transferred
+    ///
+    /// Size[bytes]: 8
     pub value: u64,
+    // Script [IGNORED] - Size[bytes]: CompactSize
 }
 
-/// Sapling input.
+/// spend is a Sapling Spend Description as described in 7.3 of the Zcash
+/// protocol specification.
 #[derive(Debug)]
 pub struct Spend {
+    // Cv [IGNORED] - Size[bytes]: 32
+    // Anchor [IGNORED] - Size[bytes]: 32
+    /// A nullifier to a sapling note.
+    ///
+    /// Size[bytes]: 32
     pub nullifier: Vec<u8>,
+    // Rk [IGNORED] - Size[bytes]: 32
+    // Zkproof [IGNORED] - Size[bytes]: 192
+    // SpendAuthSig [IGNORED] - Size[bytes]: 64
 }
 
-/// Sapling output.
+/// output is a Sapling Output Description as described in section 7.4 of the
+/// Zcash protocol spec.
 #[derive(Debug)]
 pub struct Output {
+    // Cv [IGNORED] - Size[bytes]: 32
+    /// U-coordinate of the note commitment, derived from the note's value, recipient, and a
+    /// random value.
+    ///
+    /// Size[bytes]: 32
     pub cmu: Vec<u8>,
+    /// Ephemeral public key for Diffie-Hellman key exchange.
+    ///
+    /// Size[bytes]: 32
     pub ephemeral_key: Vec<u8>,
+    /// Encrypted transaction details including value transferred and an optional memo.
+    ///
+    /// Size[bytes]: 580
     pub enc_ciphertext: Vec<u8>,
+    // OutCiphertext [IGNORED] - Size[bytes]: 80
+    // Zkproof [IGNORED] - Size[bytes]: 192
 }
 
+/// joinSplit is a JoinSplit description as described in 7.2 of the Zcash
+/// protocol spec. Its exact contents differ by transaction version and network
+/// upgrade level. Only version 4 is supported, no need for proofPHGR13.
+///
+/// NOTE: Legacy, no longer used but included for consistency.
 #[derive(Debug)]
 pub struct JoinSplit {
-    // Define fields based on your needs
+    //vpubOld [IGNORED] - Size[bytes]: 8
+    //vpubNew [IGNORED] - Size[bytes]: 8
+    //anchor [IGNORED] - Size[bytes]: 32
+    //nullifiers [IGNORED] - Size[bytes]: 64/32
+    //commitments [IGNORED] - Size[bytes]: 64/32
+    //ephemeralKey [IGNORED] - Size[bytes]: 32
+    //randomSeed [IGNORED] - Size[bytes]: 32
+    //vmacs [IGNORED] - Size[bytes]: 64/32
+    //proofGroth16 [IGNORED] - Size[bytes]: 192
+    //encCiphertexts [IGNORED] - Size[bytes]: 1202
 }
 
-/// Orchard actions.
+/// An Orchard action.
 #[derive(Debug)]
 pub struct Action {
+    // Cv [IGNORED] - Size[bytes]: 32
+    /// A nullifier to a orchard note.
+    ///
+    /// Size[bytes]: 32
     pub nullifier: Vec<u8>,
+    // Rk [IGNORED] - Size[bytes]: 32
+    /// X-coordinate of the commitment to the note.
+    ///
+    /// Size[bytes]: 32
     pub cmx: Vec<u8>,
+    /// Ephemeral public key.
+    ///
+    /// Size[bytes]: 32
     pub ephemeral_key: Vec<u8>,
+    /// Encrypted details of the new note, including its value and recipient's data.
+    ///
+    /// Size[bytes]: 580
     pub enc_ciphertext: Vec<u8>,
+    // OutCiphertext [IGNORED] - Size[bytes]: 80
 }
 
-/// Raw transactrion.
+/// Full Zcash Transactrion data.
 #[derive(Debug)]
 pub struct TransactionData {
+    /// Indicates if the transaction is an Overwinter-enabled transaction.
+    ///
+    /// Size[bytes]: [in 4 byte header]
     pub f_overwintered: bool,
+    /// The transaction format version.
+    ///
+    /// Size[bytes]: [in 4 byte header]
     pub version: u32,
+    /// Version group ID, used to specify transaction type and validate its components.
+    ///
+    /// Size[bytes]: 4
     pub n_version_group_id: u32,
+    /// Consensus branch ID, used to identify the network upgrade that the transaction is valid for.
+    ///
+    /// Size[bytes]: 4
     pub consensus_branch_id: u32,
+    /// List of transparent inputs in a transaction.
+    ///
+    /// Size[bytes]: Vec<40+CompactSize>
     pub transparent_inputs: Vec<TxIn>,
+    /// List of transparent outputs in a transaction.
+    ///
+    /// Size[bytes]: Vec<8+CompactSize>
     pub transparent_outputs: Vec<TxOut>,
+    // NLockTime [IGNORED] - Size[bytes]: 4
+    // NExpiryHeight [IGNORED] - Size[bytes]: 4
+    // ValueBalanceSapling [IGNORED] - Size[bytes]: 8
+    /// List of shielded spends from the Sapling pool
+    ///
+    /// Size[bytes]: Vec<384>
     pub shielded_spends: Vec<Spend>,
+    /// List of shielded outputs from the Sapling pool
+    ///
+    /// Size[bytes]: Vec<948>
     pub shielded_outputs: Vec<Output>,
+    /// List of JoinSplit descriptions in a transaction, no longer supported.
+    ///
+    /// Size[bytes]: Vec<1602-1698>
     pub join_splits: Vec<JoinSplit>,
+    //joinSplitPubKey [IGNORED] - Size[bytes]: 32
+    //joinSplitSig [IGNORED] - Size[bytes]: 64
+    //bindingSigSapling [IGNORED] - Size[bytes]: 64
+    ///List of Orchard actions.
+    ///
+    /// Size[bytes]: Vec<820>
     pub orchard_actions: Vec<Action>,
 }
 
-/// Zingo-Proxy transaction data.
+/// Zingo-Proxy struct for a full zcash transaction.
 #[derive(Debug)]
 pub struct FullTransaction {
+    /// Full transaction data.
     pub raw_transaction: TransactionData,
+
+    /// Raw transaction bytes.
     pub raw_bytes: Vec<u8>,
+
+    /// Transaction Id, fetched using get_block JsonRPC with verbose = 1.
     pub tx_id: Vec<u8>,
 }
 
