f1ae29595a22f766b6f6ff2248aa981938d77c57
zancas <zancas@zingolabs.org>
2025-07-23 23:52:49 -0600

    minor cleanups

--- utils/link_to_binaries.sh
+++ utils/link_to_binaries.sh
@@ -42,7 +42,7 @@ if [ -d "$BINS_DIR" ]; then
     echo "Binary setup complete. Contents of $BINS_DIR:"
     ls -la "$BINS_DIR"
 else
-    echo "Info: $BINS_DIR directory does not exist, will be created when binaries are first accessed"
+    echo "Info: $BINS_DIR will be created when binaries are first accessed"
 fi
 
 # Execute any command passed to the entrypoint
