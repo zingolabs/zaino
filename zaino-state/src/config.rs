45d0bd4c51b7d3f800c7eeab4905770e4e49daaa
zancas <zancas@zingolabs.org>
2025-09-21 21:02:26 -0700

    continue eliding network from zaino-common

--- zaino-state/src/config.rs
+++ zaino-state/src/config.rs
@@ -1,6 +1,6 @@
 //! Holds config data for Zaino-State services.
 
-use zaino_common::{Network, ServiceConfig, StorageConfig};
+use zaino_common::{ServiceConfig, StorageConfig};
 
 #[derive(Debug, Clone, serde::Deserialize, PartialEq, Copy)]
 #[serde(rename_all = "lowercase")]
