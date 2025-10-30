201ca4cc2801530aa9c509c0d5516ac32491809d
Oscar Pepper <oscarpepper@zingolabs.org>
2025-10-30 13:26:26 +0000

    cargo fmt and remove last unused import

--- integration-tests/tests/test_vectors.rs
+++ integration-tests/tests/test_vectors.rs
@@ -18,12 +18,9 @@ use zaino_state::write_u32_le;
 use zaino_state::write_u64_le;
 use zaino_state::CompactSize;
 use zaino_state::{BackendType, ChainWork, IndexedBlock};
-use zaino_state::{
-    StateService, ZcashIndexer,
-};
+use zaino_state::{StateService, ZcashIndexer};
 use zaino_testutils::from_inputs;
 use zaino_testutils::test_vectors::transactions::get_test_vectors;
-use zaino_testutils::Validator as _;
 use zaino_testutils::{TestManager, ValidatorKind};
 use zebra_chain::serialization::{ZcashDeserialize, ZcashSerialize};
 use zebra_rpc::methods::GetAddressUtxos;
