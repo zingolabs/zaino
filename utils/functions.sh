79e16440f714552dfafc9c30ea671db2f54f4e7d
nachog00 <nachogeo@zingolabs.org>
2026-03-16 14:34:46 -0300

    refactor: consolidate testing directories
    - Move test_environment/ to testing/ci-image/
    - Move test_binaries/bins/ to testing/binaries/
    - Move externals/ to testing/externals/ (for building zcashd/zebrad locally)
    - Remove obsolete temp/ directory
    - Update all references in CI, Makefile.toml, utils, and docs

--- utils/functions.sh
+++ utils/functions.sh
@@ -5,5 +5,5 @@ get_docker_hash() {
   local git_root
   git_root=$(git rev-parse --show-toplevel)
   cd "$git_root"
-  git ls-tree HEAD test_environment | git hash-object --stdin | cut -c1-14
+  git ls-tree HEAD testing/ci-image | git hash-object --stdin | cut -c1-14
 }
