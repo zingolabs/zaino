da7c8877fed6f8774abb93ca5e95235f94f1e76a
zancas <zancas@zingolabs.org>
2026-04-09 14:32:59 -0700

    update utils to .utils references

--- .utils/precommit-check.sh
+++ .utils/precommit-check.sh
@@ -1,1 +1,1 @@
-cargo check --all-features && cargo check --tests --all-features && cargo fmt && cargo clippy && ./utils/trailing-whitespace.sh reject
+cargo check --all-features && cargo check --tests --all-features && cargo fmt && cargo clippy && ./.utils/trailing-whitespace.sh reject
