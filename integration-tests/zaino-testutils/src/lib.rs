873f02a55401fab70fc95fd004a01e658b5711b0
alexxiy <alexey.savik@gmail.com>
2026-04-10 20:55:33 +0300

    Add donation_address feature
    (cherry picked from commit 9eb2375e8662f48d3711119136b31c4c9143fc0b)

--- integration-tests/zaino-testutils/src/lib.rs
+++ integration-tests/zaino-testutils/src/lib.rs
@@ -421,6 +421,7 @@ where
                 },
                 zebra_db_path,
                 network: zaino_network_kind,
+                donation_address: None,
             };
 
             let (handle, service_subscriber) = Indexer::<Service>::launch_inner(
