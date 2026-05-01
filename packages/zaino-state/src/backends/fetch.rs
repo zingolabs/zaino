978fc93f6d57706a7ab48580683d5db2986baee3
Pacu <pacu@zecdev.org>
2026-05-01 12:52:36 +0200

    Add gettxoutsetinfo state passthrough

--- packages/zaino-state/src/backends/fetch.rs
+++ packages/zaino-state/src/backends/fetch.rs
@@ -36,7 +36,7 @@ use zaino_fetch::{
             z_validate_address::{
                 ZValidateAddressResponse, DEPRECATION_NOTICE as Z_VALIDATE_DEPRECATION,
             },
-            GetMempoolInfoResponse, GetNetworkSolPsResponse,
+            GetMempoolInfoResponse, GetNetworkSolPsResponse, GetTxOutSetInfoResponse,
         },
     },
 };
@@ -452,6 +452,15 @@ impl ZcashIndexer for FetchServiceSubscriber {
 
     async fn get_mining_info(&self) -> Result<GetMiningInfoWire, Self::Error> {
         Ok(self.fetcher.get_mining_info().await?)
+    }
+
+    /// Returns statistics about the unspent transaction output set.
+    ///
+    /// zcashd reference: [`gettxoutsetinfo`](https://zcash.github.io/rpc/gettxoutsetinfo.html)
+    /// method: post
+    /// tags: blockchain
+    async fn get_tx_out_set_info(&self) -> Result<GetTxOutSetInfoResponse, Self::Error> {
+        Ok(self.fetcher.get_tx_out_set_info().await?)
     }
 
     /// Returns the hash of the best block (tip) of the longest chain.
